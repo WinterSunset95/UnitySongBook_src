5f6b3e18176def2a5af70c3f9f7cda7bee3425a7
WinterSunset95 <wintersunset95@gmail.com>
2022-05-29 18:46:00 +0530

    saving the current state

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SongPage from './app/components/SongPage';
+import HomeLoadingPage from './app/components/HomeLoadingPage';
+import Home from './app/Home'
+
+const Stack = createNativeStackNavigator()
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Home navigation={navigation}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+function HomeLoadingScreen({navigation}) {
+  return (
+    <View>
+      <HomeLoadingPage navigation={navigation} />
+    </View>
+  )
+}
+function SongScreen({route}) {
+  const {title, composer, link} = route.params
+  return (
+    <View style={styles.container}>
+      <SongPage title={title} composer={composer} link={link}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Song" component={SongScreen}/>
+	<Stack.Screen name="LoadHome" component={HomeLoadingScreen} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -14,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
 });
