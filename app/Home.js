d7d37c1707500b594f9943ea1343b9ca78e5ede9
WinterSunset95 <wintersunset95@gmail.com>
2022-05-30 07:42:13 +0530

    almost there

--- app/Home.js
+++ app/Home.js
@@ -1,21 +1,62 @@
 import React, {useEffect, useState} from 'react'
 import { FontAwesome } from '@expo/vector-icons'
-import { StyleSheet, Text, View, ScrollView, Image, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image, TextInput, TouchableOpacity, Keyboard } from 'react-native'
 import Song from './Song'
 
 export default function Home({navigation}) {
   const [json, setJson] = useState()
+  const [search, setSearch] = useState("")
+  const [text, setText] = useState("")
+  const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
   const switchScreen = () => {
     navigation.navigate('LoadHome')
   }
- 
+  function GetSortOrder(prop) {    
+    return function(a, b) {    
+      if (a[prop] > b[prop]) {    
+          return 1;    
+      } else if (a[prop] < b[prop]) {    
+          return -1;    
+      }    
+      return 0;    
+    }    
+  }
   async function fetch_autumn() {
     let response = await fetch('https://wintersunset95.github.io/UnitySongBook/list.json')
     let my_json = await response.json()
     work_with_json(my_json)
   }
   function work_with_json(file) {
-    setJson(file)
+    file.sort(GetSortOrder("title"))
+    for(let i = 0; i<file.length; i++) {
+      item = file[i]
+      title_string = item.title
+      title_index = title_string.charAt(0)
+      item["class"] = title_index
+    }
+    for(let i = 0; i<file.length; i++){
+      item = file[i]
+      const search_text = search.toLowerCase()
+      const length = search.length
+      const title = item.title
+      const title_formatted = title.toLowerCase()
+      const to_match = title_formatted.slice(0, length)
+      if (search_text===to_match) {
+	item['index'] = true
+      }
+      else {
+	item['index'] = false
+      }
+    }
+    const results = file.filter(function(item) {
+      return item.index
+    })
+    try {
+      setJson(results)
+    }
+    catch(err) {
+      setJson(file)
+    }
   }
   fetch_autumn()
   try {
@@ -36,10 +77,18 @@ export default function Home({navigation}) {
 	  <TextInput 
 	    placeholder={'Search'}
 	    style={[styles.headerSubItem, styles.inputField]}
+	    onChangeText={text => {
+	      setSearch(text)
+	    }}
 	  />
-	  <FontAwesome name="search" size={24} color="black" style={styles.headerSubItem} />
+	  <TouchableOpacity onPress={() => {
+	    Keyboard.dismiss()
+	  }} >
+	    <FontAwesome name="search" size={24} color="black" style={styles.headerSubItem}/>
+	  </TouchableOpacity>
 	</View>
       </View>
+      
       <ScrollView style={styles.body}>
 	{
 	  songList.map(({title, composer, link}) => {
@@ -87,6 +136,7 @@ const styles = StyleSheet.create({
     borderColor: 'rgba(200,200,200,0.5)',
     borderRadius: 5,
     borderWidth: 2, 
+    paddingHorizontal: 10,
   },
   logo_image: {
     width: 60,
--- app/components/SongPage.js
+++ app/components/SongPage.js
@@ -4,7 +4,7 @@ import { Feather } from '@expo/vector-icons';
 
 export default function SongPage({title, composer, link}) {
   const [lyrics, setLyrics] = useState()
-  const [zoom, setZoom] = useState(15)
+  const [zoom, setZoom] = useState(20)
   async function getLyrics() {
     let response = await fetch(link)
     let my_json = await response.text()
@@ -17,7 +17,7 @@ export default function SongPage({title, composer, link}) {
     <View style={styles.container}>
       <View style={styles.header}>
 	<Text style={styles.title}>{title}</Text>
-	<Text style={styles.composer}>{composer}</Text>
+	<Text style={styles.composer}> - {composer}</Text>
       </View>
       <View style={styles.body}>
 	<Text style={{fontSize: zoom, padding: 10}}>{lyrics}</Text>
@@ -35,8 +35,6 @@ export default function SongPage({title, composer, link}) {
 const styles = StyleSheet.create({
   body: {
     minHeight: '50%', 
-    justifyContent: 'center',
-    alignItems: 'center',
     marginTop: 50,
   },
   container: {
@@ -44,14 +42,14 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   composer: {
-    fontSize: 10,
+    fontSize: 15,
     marginHorizontal: 5,
   },
   header: {
     alignItems: 'flex-end'
   },
   title: {
-    fontSize: 25,
+    fontSize: 30,
     margin: 5,
   },
   zoomer: {
