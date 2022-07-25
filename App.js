88e5f9cae97d8e862fc9ec20f4aba5a818c3a5b8
WinterSunset95 <wintersunset95@gmail.com>
2022-07-25 21:33:00 +0530

    totally changed the way the screen is rendered

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SongPage from './app/components/SongPage';
 import Home from './app/Home'
 import Pager from './app/components/Pager'
+import { WebView } from 'react-native-webview'
 
 const Stack = createNativeStackNavigator()
 
@@ -38,13 +39,17 @@ function PagerScreen({route}) {
 
 export default function App() {
   return (
-    <NavigationContainer style={styles.container}>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="Song" component={SongScreen}/>
-	      <Stack.Screen name="Pager" component={PagerScreen} options={{headerShown: false}}/> 
-      </Stack.Navigator>
-    </NavigationContainer>
+//    <NavigationContainer style={styles.container}>
+//      <Stack.Navigator>
+//        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+//        <Stack.Screen name="Song" component={SongScreen}/>
+//	      <Stack.Screen name="Pager" component={PagerScreen} options={{headerShown: false}}/> 
+//      </Stack.Navigator>
+//    </NavigationContainer>
+    <View style={styles.container}>
+      <WebView source={{ uri: "https://unity-song-book-web.vercel.app" }} />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
